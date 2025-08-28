633240149146ce48bf53535c86de3ca444e0e943
Mykhail <its.astral.step@gmail.com>
2025-08-28 13:20:29 +0300

    Test №2 'Login with invalid password'

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -2,6 +2,9 @@ class LoginPage {
     get inputUsername() { return $('[data-test="username"]'); }
     get inputPassword() { return $('[data-test="password"]'); }
     get btnLogin() { return $('[data-test="login-button"]'); }
+    get errorMessage() { return $('[data-test="error"]'); }
+    get usernameErrorIcon() { return $('input#user-name ~ svg.error_icon'); }
+    get passwordErrorIcon() { return $('input#password ~ svg.error_icon'); }
 
     async open() {
         await browser.url('https://www.saucedemo.com/');
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -25,4 +25,39 @@ describe('Login functionality', () => {
         await expect(InventoryPage.cartIcon).toBeDisplayed();
        
     });
+
+    it('Login with invalid password', async () => {
+    
+        await LoginPage.open();
+      
+        await LoginPage.inputUsername.setValue('standard_user');
+       
+        await expect(LoginPage.inputUsername).toHaveValue('standard_user');
+        
+        await LoginPage.inputPassword.setValue('not_a_secret_sauce');
+        
+        const typeAttr = await LoginPage.inputPassword.getAttribute('type');
+        await expect(typeAttr).toEqual('password');
+
+        await LoginPage.btnLogin.click();
+    
+        await expect(LoginPage.errorMessage).toBeDisplayed();
+        await expect(LoginPage.errorMessage).toHaveText(
+            'Epic sadface: Username and password do not match any user in this service');
+
+        const usernameClass = await LoginPage.inputUsername.getAttribute('class');
+        await expect(usernameClass).toContain('input_error form_input error');
+
+        const passwordClass = await LoginPage.inputPassword.getAttribute('class');
+        await expect(passwordClass).toContain('input_error form_input error')
+
+        const usernameBorderColor = await LoginPage.inputUsername.getCSSProperty('border-bottom-color');
+        await expect(usernameBorderColor.parsed.hex).toBe('#e2231a');
+
+        const passwordBorderColor = await LoginPage.inputPassword.getCSSProperty('border-bottom-color');
+        await expect(passwordBorderColor.parsed.hex).toBe('#e2231a');
+
+        await expect(LoginPage.usernameErrorIcon).toBeDisplayed();
+        await expect(LoginPage.passwordErrorIcon).toBeDisplayed();
+    });
 });
